fcf139571939b7bbc319036b0798fd7fb09ace49
Guylian Cox <guylian.cox@pictawall.com>
2016-09-29 12:54:37 +0200

    Added targeted browsers to browserstack test setup

--- karma/browserstack.karma.conf.js
+++ karma/browserstack.karma.conf.js
@@ -22,23 +22,52 @@ if (process.env.BROWSERSTACK_USERNAME && process.env.BROWSERSTACK_ACCESS_KEY) {
 }
 
 conf.customLaunchers = {
-  bsMacFirefox21: {
+  bsMacFirefox14: {
     base: 'BrowserStack',
     browser: 'firefox',
-    browser_version: '21.0',
+    browser_version: '33.0',
+    os: 'OS X',
+    os_version: 'El Capitan',
+  },
+  bsMacChrome38: {
+    base: 'BrowserStack',
+    browser: 'chrome',
+    browser_version: '38.0',
+    os: 'OS X',
+    os_version: 'El Capitan',
+  },
+  bsMacSafari6: {
+    base: 'BrowserStack',
+    browser: 'safari',
+    browser_version: '6.2',
     os: 'OS X',
     os_version: 'Mountain Lion',
   },
-  bsMacFirefox48: {
+  bsMacOpera20: {
     base: 'BrowserStack',
-    browser: 'firefox',
-    browser_version: '48.0',
+    browser: 'opera',
+    browser_version: '20.0',
     os: 'OS X',
     os_version: 'El Capitan',
-  }
+  },
+  bsWinEdge13: {
+    base: 'BrowserStack',
+    browser: 'edge',
+    browser_version: '13',
+    os: 'WINDOWS',
+    os_version: '10',
+  },
+  bsWinIe11: {
+    base: 'BrowserStack',
+    browser: 'ie',
+    browser_version: '11',
+    os: 'WINDOWS',
+    os_version: '7',
+  },
 };
 
-conf.browsers = ['bsMacFirefox48'];
+conf.browsers = ['bsMacFirefox14', 'bsMacChrome38', 'bsMacSafari6', 'bsMacOpera20', 'bsWinEdge13', 'bsWinIe11'];
+// conf.browsers = ['bsWinIe11'];
 
 module.exports = function configureKarma(config) {
   config.set(conf);
